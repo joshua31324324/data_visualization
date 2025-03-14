8860820957632209d50ce86084360df878608991
joshua <jo336@njit.edu>
2025-03-14 16:48:24 -0400

    added background

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { parseCSV } from '../utils/parseCSV';
 import DataTable from './dataTable';
+import './dashboard.css';
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
@@ -11,9 +12,14 @@ const Dashboard = () => {
   };
 
   return (
-    <div>
-      <input type="file" accept=".csv" onChange={handleFileUpload} />
-      <DataTable data={data} />
+    <div className="dashboard-container">
+      <div className="background-gif">
+        <h1>CSV Data Viewer</h1>
+        <p>Click on the button below to upload a CSV file. Make sure the file is properly formatted so you can view its data.</p>
+        <p>Only CSV files are allowed</p>
+        <input type="file" accept=".csv" onChange={handleFileUpload} />
+        <DataTable data={data} />
+        </div>
     </div>
   );
 };
